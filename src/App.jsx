21d64d55c1a77c2c63517b946202f7bd63023cb1
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2024-11-08 11:13:48 +0600

    hero done

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ import Vision from "./components/Vision";
 
 const App = () => {
   return (
-    <main className="bg-[#1A1E1C] bg-[url(/bg.png)] bg-no-repeat bg-cover pb-[569px]">
-      <div className="w-full max-w-[1440px] px-24 mx-auto">
+    <main className="bg-[#1A1E1C] bg-[url(/bg.svg)] bg-no-repeat bg-cover pb-[569px] overflow-x-hidden">
+      <div className="w-full max-w-[1440px] px-2.5 md:px-10 lg:px-20 xl:px-24 mx-auto ">
         <Header />
 
-        <div className="my-16  space-y-20">
+        <div className="space-y-20">
           <Hero />
           <Vision />
           <DebitCard />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,27 +1,28 @@
-import heroCard from "../assets/hero-card.svg";
-
 const Hero = () => {
-	return (
-		<section className='flex gap-8'>
-			<div className='space-y-6 w-1/2'>
-				<h1 className='font-bold leading-tight text-5xl'>
-					Spend your Cardano anywhere, anytime.
-				</h1>
+  return (
+    <section className="flex flex-col-reverse sm:flex-row gap-10 lg:gap-2.5 xl:gap-8 justify-between items-center mt-10 lg:mt-20 xl:mt-[128px]">
+      <div className="space-y-6 sm:w-1/2">
+        <h1 className="font-bold leading-tight text-2xl md:text-3xl lg:text-4xl xl:text-5xl">
+          Spend your Cardano anywhere, anytime.
+        </h1>
 
-				<p className='text-secondary w-4/5'>
-					Our user-friendly platform enables businesses and
-					individuals to seamlessly convert and spend their crypto for
-					everyday purchases.
-				</p>
+        <p className="text-secondary w-4/5 text-xs lg:text-sm xl:text-base">
+          Our user-friendly platform enables businesses and individuals to
+          seamlessly convert and spend their crypto for everyday purchases.
+        </p>
 
-				<button className='bg-primary font-bold px-8 py-3 rounded-3xl text-sm'>
-					Get Started
-				</button>
-			</div>
+        <button className="bg-primary font-bold px-8 py-3 rounded-3xl text-sm">
+          Get Started
+        </button>
+      </div>
 
-			<img className='w-1/2' src={heroCard} alt='hero card' />
-		</section>
-	);
+      <img
+        className="xl:-mr-[130px] w-10/12 sm:w-1/2"
+        src="/hero-card.png"
+        alt="hero card"
+      />
+    </section>
+  );
 };
 
 export default Hero;
